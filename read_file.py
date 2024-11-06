65168709e89b3e810d931828fec95f52d4b254cc
djp1-hub <69582216+djp1-hub@users.noreply.github.com>
2024-11-06 18:52:43 +0100

    Удалены лишние операции

--- read_file.py
+++ read_file.py
@@ -7,18 +7,12 @@ start_time = datetime.now()
 # Чтение всех листов в один словарь DataFrames
 excel_sheets = pd.read_excel(file_path, sheet_name=None)
 
-# Дополнительное (и лишнее) чтение каждого листа
 # Основное объединение всех листов
 united_df = pd.concat([df for df in excel_sheets.values()])
 
-# Лишний расчет времени для каждой части отдельно
-end_time_intermediate = datetime.now()
-print(f'Intermediate Duration: {(end_time_intermediate - start_time).total_seconds()} seconds')
 
 # Группировка по дате продажи и суммирование количества
-final_start_time = datetime.now()
 grouped_df = united_df.groupby("Дата продажи")["Количество"].sum()
-final_end_time = datetime.now()
 
 # Общий расчет времени выполнения
 print(grouped_df)
